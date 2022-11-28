c0fff86070d975b94f8f6b80016603cdf75f3fd3
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-28 19:21:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include "duration.h"
+#include <iostream>
+#include <cassert>
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
+
+    Duration dur;
+    
+    std::cout << dur.getDuration() << "\n";
+
+
+    Duration d(5);
+
+    assert(d.getDuration() == 5);
+
+    d.tick();
+
+    assert(d.getDuration() == 6);
+
+    d.tick(3);
+
+    assert(d.getDuration() == 9);
+
+
+
+
     return 0;
 }
